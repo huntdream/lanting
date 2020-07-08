28627b8dc5780b88fe8bb8094e4381773661e787
Yu Mao <maoyu960320@hotmail.com>
2020-07-08 11:56:13 +0800

    Updates

---
+++ precache-manifest.7b63ff97c6af4a540e41668a5ef26c94.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24edab9462ab5c25c9c75239377d7855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12993757645044a95930",
+    "url": "/static/css/main.b8c44fb3.chunk.css"
+  },
+  {
+    "revision": "41619715d4232e2e29fd",
+    "url": "/static/js/2.e8d4fad8.chunk.js"
+  },
+  {
+    "revision": "5f2b4cf7cfe4dd282251aade22b543c9",
+    "url": "/static/js/2.e8d4fad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12993757645044a95930",
+    "url": "/static/js/main.5dec1932.chunk.js"
+  },
+  {
+    "revision": "a7089f56ba19c12bc39e",
+    "url": "/static/js/runtime-main.247f53a4.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
