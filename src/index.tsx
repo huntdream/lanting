7b05adbf55090640bc913b1d1dd10d6fac6fd4a2
Yu Mao <maoyu960320@hotmail.com>
2020-07-28 22:14:44 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
 import './index.scss';
-import Apollo from 'components/Apollo';
 import App from './containers/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Apollo>
+    <RecoilRoot>
       <App />
-    </Apollo>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
