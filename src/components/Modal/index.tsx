39172271e29b6208ac4a15895f2b1e35c5f25c9f
Yu Mao <maoyu960320@hotmail.com>
2020-07-31 18:32:18 +0800

    add Modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect, ReactNode, MouseEvent } from 'react';
+import cls from 'classnames';
 import './style.scss';
+import { createPortal } from 'react-dom';
+import Icon from 'components/Icon';
+import Button from 'components/Button';
+import useScrollLock from 'hooks/useScrollLock';
 
-interface ModalProps {}
+interface ModalProps {
+  visible?: boolean;
+  className?: string;
+  title?: ReactNode;
+  onClose?: () => void;
+}
 
-const Modal: React.FC<ModalProps> = () => {
-  return <div>Modal</div>;
+const Modal: React.FC<ModalProps> = ({
+  visible,
+  onClose,
+  title,
+  className,
+  children,
+}) => {
+  useScrollLock(visible);
+
+  const stopPropagation = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+  };
+
+  return visible
+    ? createPortal(
+        <div className='lanting-modal'>
+          <div className='lanting-modal-mask'></div>
+          <div className='lanting-modal-wrapper' onClick={onClose}>
+            <div className='lanting-modal-inner'>
+              <div className='lanting-modal-main' onClick={stopPropagation}>
+                <div className={cls('lanting-modal-body', className)}>
+                  <div className='lanting-modal-header'>
+                    <h2 className='lanting-modal-title'>{title}</h2>
+                    <Button
+                      className='lanting-modal-close'
+                      color='secondary'
+                      onClick={onClose}
+                    >
+                      <Icon className='lanting-modal-close-icon'>close</Icon>
+                    </Button>
+                  </div>
+                  <div className='lanting-modal-content'>{children}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>,
+        document.getElementById('root')!
+      )
+    : null;
 };
 
 export default Modal;
