1840b625dbe125ab21082846147482bc3d31d275
Yu Mao <maoyulore@outlook.com>
2022-05-07 22:28:19 +0800

    Divider

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+interface Props {}
+
+const Divider: React.FC<Props> = () => {
+  return <div className='lanting-divider'></div>;
+};
+
+export default Divider;
