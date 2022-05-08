77549b0013b3bc35e2cbdb55bda0bf4c93ccb381
Yu Mao <maoyulore@outlook.com>
2022-05-08 20:38:43 +0800

    improve Exception

--- src/components/Exception/index.tsx
+++ src/components/Exception/index.tsx
@@ -1,16 +1,19 @@
+import React, { ReactNode } from 'react';
 import Icon from 'components/Icon';
-import React from 'react';
 import './style.scss';
 
-interface ExceptionProps {}
+interface ExceptionProps {
+  children?: ReactNode;
+}
 
-const Exception: React.FC<ExceptionProps> = () => {
+const Exception: React.FC<ExceptionProps> = ({ children }) => {
   return (
     <div className='lanting-exception'>
       <div className='lanting-exception-icon'>
         <Icon>error_outline</Icon>
       </div>
       <div className='lanting-exception-text'>Something went wrong</div>
+      {children}
     </div>
   );
 };
