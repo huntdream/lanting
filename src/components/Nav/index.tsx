56ee5e9e6c831b7216908f47150fddf59abb87ba
Yu Mao <maoyulore@outlook.com>
2022-05-10 23:18:24 +0800

    Add account icon

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import classNames from 'classnames';
 import './style.scss';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Icon from 'components/Icon';
 
 import ThemeToggle from 'components/ThemeToggle';
@@ -10,10 +10,15 @@ interface NavProps {}
 
 const Nav: React.FC<NavProps> = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   const isEditPage = useMemo(() => {
     return pathname === '/edit';
   }, [pathname]);
+
+  const handleAccountClick = () => {
+    navigate('/signin');
+  };
 
   return (
     <nav className={classNames(`lanting-nav`)}>
@@ -30,6 +35,7 @@ const Nav: React.FC<NavProps> = () => {
           </Link>
         )}
         <ThemeToggle />
+        <Icon onClick={handleAccountClick}>account_circle</Icon>
       </div>
     </nav>
   );
