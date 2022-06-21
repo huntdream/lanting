456aa3ce7498dab0a2ca64c10b8de1c974be5948
Yu Mao <maoyulore@outlook.com>
2022-06-21 12:59:46 +0800

    Support timeout

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -10,36 +10,51 @@ import './style.scss';
 
 interface Props {
   title: ReactNode;
+  timeout?: number;
   children: ReactElement;
 }
 
-const Tooltip: React.FC<Props> = ({ children, title }) => {
+const Tooltip: React.FC<Props> = ({ children, title, timeout = 0 }) => {
   const ref = useRef<HTMLDivElement>(null);
+  const timer = useRef<number>(0);
   const [visible, setVisible] = useState(false);
   const [position, setPosition] = useState({ left: 0, top: 0 });
 
   const renderTooltip = () => {
     if (visible) {
-      return <div className='lanting-tooltip-content'>{title}</div>;
+      return (
+        <div className='lanting-tooltip-content' style={{ opacity: 1 }}>
+          {title}
+        </div>
+      );
     }
   };
 
   const onMouseEnter = (event: MouseEvent) => {
-    console.log('enter', event.target);
     if (!visible) {
       const target = event.target as HTMLElement;
       const { top, left, height, width } = target.getBoundingClientRect();
 
       const x = left + window.scrollX + width / 2;
       const y = top - height + window.scrollY;
-      setPosition({ left: x, top: y - 18 });
+      setPosition({ left: x, top: y - 12 });
 
-      setVisible(true);
+      if (timeout > 0) {
+        timer.current = window.setTimeout(() => {
+          setVisible(true);
+        }, timeout);
+      } else {
+        setVisible(true);
+      }
     }
   };
 
   const onMouseLeave = () => {
     setVisible(false);
+
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
   };
 
   const tooltip = (
