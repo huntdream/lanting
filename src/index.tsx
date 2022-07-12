c8161f2550d1f7f38d8918c79e1dde87d913d5c7
Yu Mao <maoyulore@outlook.com>
2022-07-12 21:21:13 +0800

    Replace local material icons font with npm source

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.scss';
 import App from './containers/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 import request from 'utils/request';
+import '@material-design-icons/font/filled.css';
+import './index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
