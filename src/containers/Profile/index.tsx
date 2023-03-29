5374f3bac26a3b27545919fdb742f4654b050cbb
Yu Mao <maoyulore@outlook.com>
2023-03-29 14:03:06 +0800

    User profile

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,16 +1,25 @@
 import Avatar from 'components/Avatar';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useSWR from 'swr';
+import { IUser } from 'typing/user';
 import './style.scss';
 
 interface Props {}
 
 const Profile: React.FC<Props> = () => {
+  const { id } = useParams();
+  const { data } = useSWR<IUser>(`/user/${id}`);
+
+  console.log(data);
+
   return (
     <div className='lanting-profile'>
       <div className='lanting-profile-header'>
         <div className='lanting-profile-avatar'>
-          <Avatar />
+          <Avatar src={data?.avatar} />
         </div>
+        <div className='lanting-profile-name'>{data?.name}</div>
       </div>
     </div>
   );
