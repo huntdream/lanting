567bc10b4753beec72ab36bbf528978d78ab07a4
Yu Mao <maoyulore@outlook.com>
2023-04-18 09:02:33 +0800

    Fix typing

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -18,7 +18,7 @@ interface FormValues {
   password: string;
 }
 
-const Schema: Yup.SchemaOf<FormValues> = Yup.object({
+const Schema: Yup.Schema<FormValues> = Yup.object({
   username: Yup.string()
     .min(5, 'Username must be between 5 and 16 characters long.')
     .max(16, 'Username must be between 6 and 16 characters long.')
