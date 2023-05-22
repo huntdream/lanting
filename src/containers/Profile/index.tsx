02b84a876a96740e369fd2fc33858c2b6399b975
Yu Mao <maoyulore@outlook.com>
2023-05-22 16:00:42 +0800

    Add Tabs

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -4,23 +4,48 @@ import { useParams } from 'react-router-dom';
 import useSWR from 'swr';
 import { IUser } from 'typing/user';
 import './style.scss';
+import Tabs, { Tab } from 'components/Tabs';
+import Feed from 'containers/Feed';
 
 interface Props {}
 
 const Profile: React.FC<Props> = () => {
   const { id } = useParams();
-  const { data } = useSWR<IUser>(`/user/${id}`);
+  const { data } = useSWR<IUser>(`/user/${id}`, {
+    refreshInterval: 0,
+    shouldRetryOnError: false,
+    revalidateOnFocus: false,
+  });
 
   console.log(data);
+
+  const tabs: Tab[] = [
+    {
+      id: 'posts',
+      label: 'Posts',
+      content: <Feed id={id} />,
+    },
+    {
+      id: 'media',
+      label: 'Media',
+      content: 'TODO: this should index media from posts',
+    },
+    {
+      id: 'about',
+      label: 'About',
+      content: <div>Something about the mysterious</div>,
+    },
+  ];
 
   return (
     <div className='lanting-profile'>
       <div className='lanting-profile-header'>
-        <div className='lanting-profile-avatar'>
-          <Avatar src={data?.avatar} />
+        <div className='lanting-profile-user'>
+          <Avatar src={data?.avatar} round size='large' />
         </div>
         <div className='lanting-profile-name'>{data?.name}</div>
       </div>
+      <Tabs tabs={tabs} activeTab='feed' />
     </div>
   );
 };
