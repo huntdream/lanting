0527f2841ec852a32c122a6e2e99e2c5f6adc6f6
Yu Mao <maoyulore@outlook.com>
2023-06-21 11:05:43 +0800

    migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+export default defineConfig(() => {
+  return {
+    server: {
+      open: true,
+    },
+    build: {
+      outDir: 'build',
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {},
+      },
+    },
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      svgrPlugin(),
+      visualizer({
+        template: 'treemap',
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+        filename: 'analyse.html',
+      }),
+    ],
+  };
+});
