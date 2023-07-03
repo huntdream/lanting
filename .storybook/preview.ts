7cc6ae3a106ff7213b9ca1a9831697fb4e273f3a
Yu Mao <maoyulore@outlook.com>
2023-07-03 11:14:14 +0800

    Integrate Storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from '@storybook/react';
+import '@material-design-icons/font/filled.css';
+import '../src/index.scss'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
