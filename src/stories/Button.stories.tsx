320431211f5eb8cffd93f241b9c101ed6fc4978d
Yu Mao <maoyulore@outlook.com>
2023-07-28 15:25:12 +0800

    Add Button's story

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from '../components/Button';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Button',
+  },
+};
