92048057ee6701b85145c4cd12ccb3729fd97e0c
huntdream <maoyulore@outlook.com>
2023-08-16 08:29:23 +0000

    Deploying to gh-pages from  @ c4054f0c54130d5ed7de8e7c6fedb42514603ff5 🚀

--- assets/Button.stories-a77c66db.js
+++
@@ -1,6 +0,0 @@
-import{B as a}from"./index-c458995d.js";import"./index-111a7888.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-d82410e1.js";const i={component:a},r={args:{children:"Button"}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    children: 'Button'
-  }
-}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const d=["Primary"];export{r as Primary,d as __namedExportsOrder,i as default};
-//# sourceMappingURL=Button.stories-a77c66db.js.map
