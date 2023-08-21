b6e04088829b61fc1f7f0417cc7a4455b8d9f646
Yu Mao <maoyulore@outlook.com>
2023-08-21 19:08:02 +0800

    Add decorators

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react';
-import '../src/index.scss'
+import '../src/index.scss';
+import React from 'react';
+import Toast from '../src/components/Toast';
 
 const preview: Preview = {
   parameters: {
@@ -11,6 +13,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <Toast>
+        <Story />
+      </Toast>
+    ),
+  ],
 };
 
 export default preview;
