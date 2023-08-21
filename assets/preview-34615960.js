559a8576e3c091be675d72ad1a81942ac7149e57
huntdream <maoyulore@outlook.com>
2023-08-21 11:09:19 +0000

    Deploying to gh-pages from  @ b6e04088829b61fc1f7f0417cc7a4455b8d9f646 🚀

--- assets/preview-34615960.js
+++
@@ -1,2 +0,0 @@
-const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
-//# sourceMappingURL=preview-34615960.js.map
