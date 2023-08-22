c6371a7a2ce93f47953a7d3de5ba9661129965c3
huntdream <maoyulore@outlook.com>
2023-08-22 11:14:34 +0000

    Deploying to gh-pages from  @ 81c52cf46dd403ccc8c7070170377f1fce5135a4 🚀

---
+++ assets/Toast.stories-99b79fb1.js
@@ -0,0 +1,27 @@
+import{j as t}from"./index-111a7888.js";import{a as d,T as u}from"./index-ac89f95a.js";import{r as m}from"./index-76fb7be0.js";import{B as r}from"./index-2f3064e6.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./index-dad6ac06.js";const x=()=>{const{toast:s}=m.useContext(d);return[s]},P={component:u},o={render:()=>{const[s]=x(),[i,c]=m.useState("top"),l=["top","top-left","top-right","bottom","bottom-left","bottom-right"];return t.jsxs("div",{children:[t.jsx("div",{children:l.map(n=>t.jsx(r,{onClick:()=>c(n),style:{textTransform:"capitalize",margin:"12px"},children:n.split("-").join(" ")},n))}),t.jsx("div",{style:{marginTop:"16px"},children:t.jsx(r,{onClick:()=>s(`This is a ${i.split("-").join(" ")} toast`,{position:i}),children:"Show Toast"})})]})}};var e,a,p;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => {
+    const [toast] = useToast();
+    const [position, setPosition] = useState<Position>('top');
+    const positions: Position[] = ['top', 'top-left', 'top-right', 'bottom', 'bottom-left', 'bottom-right'];
+    return <div>
+        <div>
+          {positions.map(p => <Button onClick={() => setPosition(p)} style={{
+          textTransform: 'capitalize',
+          margin: '12px'
+        }} key={p}>
+              {p.split('-').join(' ')}
+            </Button>)}
+        </div>
+        <div style={{
+        marginTop: '16px'
+      }}>
+          <Button onClick={() => toast(\`This is a \${position.split('-').join(' ')} toast\`, {
+          position
+        })}>
+            Show Toast
+          </Button>
+        </div>
+      </div>;
+  }
+}`,...(p=(a=o.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};const b=["Primary"];export{o as Primary,b as __namedExportsOrder,P as default};
+//# sourceMappingURL=Toast.stories-99b79fb1.js.map
--- assets/preview-8c769461.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./index-111a7888.js";import{T as t}from"./index-8bd74453.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./index-bb758768.js";const c={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}},decorators:[r=>o.jsx(t,{children:o.jsx(r,{})})]};export{c as default};
-//# sourceMappingURL=preview-8c769461.js.map
