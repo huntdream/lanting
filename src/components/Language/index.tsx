37b57f9a62d01d9bab7303fdd6bb6a6ffb9dc508
Yu Mao <maoyulore@outlook.com>
2023-08-22 09:30:42 +0800

    Add feedback after switch language

--- src/components/Language/index.tsx
+++ src/components/Language/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Icon from 'components/Icon';
+import useToast from 'components/Toast/useToast';
 
 interface Props {}
 
 type Lang = 'en-US' | 'zh-CN';
 
 const Language: React.FC<Props> = () => {
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
+  const [toast] = useToast();
 
   const handleLanguageChange = () => {
     i18n.changeLanguage(i18n.language.includes('zh') ? 'en-US' : 'zh-CN');
+    toast(t('greeting'));
   };
 
   return <Icon onClick={handleLanguageChange} name={i18n.language as Lang} />;
