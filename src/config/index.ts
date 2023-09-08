be01ef7e9deea290053f12773b2a9adce67c5a3b
Yu Mao <maoyulore@outlook.com>
2023-09-08 10:05:00 +0800

    Use wss

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,7 +9,7 @@ const config: Config = {
   baseurl: isProd
     ? 'https://api.maoyu.info/api/v1'
     : 'http://localhost:4000/api/v1',
-  yjs: isProd ? "ws://yjs.maoyu.info" : "ws://localhost:1234"
+  yjs: isProd ? "wss://yjs.maoyu.info" : "ws://localhost:1234"
 };
 
 export default config;
