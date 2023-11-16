94f60335bc3966fa330e299fdea5400177773a8e
Yu Mao <maoyulore@outlook.com>
2023-11-16 16:20:56 +0800

    Add highlight prop

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -2,14 +2,16 @@ import Avatar from 'components/Avatar';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IUser } from 'typing/user';
+import cls from 'classnames';
 import './style.scss';
 
 interface Props {
   user?: IUser;
   hideAvatar?: boolean;
+  highlight?: boolean;
 }
 
-const User: React.FC<Props> = ({ user, hideAvatar }) => {
+const User: React.FC<Props> = ({ user, hideAvatar, highlight }) => {
   const navigate = useNavigate();
 
   const handleNavigate = () => {
@@ -21,7 +23,13 @@ const User: React.FC<Props> = ({ user, hideAvatar }) => {
   return (
     <div className='lanting-user' onClick={handleNavigate}>
       {!hideAvatar && <Avatar size='small' round src={user?.avatar} />}
-      <div className='lanting-user-name'>{user?.name || user?.username}</div>
+      <div
+        className={cls('lanting-user-name', {
+          'lanting-user-name--hl': highlight,
+        })}
+      >
+        {user?.name || user?.username}
+      </div>
     </div>
   );
 };
