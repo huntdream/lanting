59d79ee4ad35f7aadf135b937f88bc96d9672697
huntdream <maoyulore@outlook.com>
2024-02-09 08:37:59 +0000

    Deploying to gh-pages from  @ 4a8a00b0ad05a4d675eca788798baf4850159958 🚀

---
+++ assets/Button.stories-xMROe5AP.js
@@ -0,0 +1,5 @@
+import{B as a}from"./index-iBUZGUaU.js";import"./jsx-runtime-vNq4Oc-g.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-sSNru9bU.js";import"./icons-UAwrDR8W.js";import"./account_circle-CWE-8K_X.js";const u={component:a},r={args:{children:"Button"}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Button'
+  }
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const l=["Primary"];export{r as Primary,l as __namedExportsOrder,u as default};
