b1c7aba1076f83495725c5166ec1d99070d0eaa0
Yu Mao <maoyulore@outlook.com>
2025-01-08 15:52:14 +0800

    Update profile info

--- src/containers/Profile/Edit/index.tsx
+++ src/containers/Profile/Edit/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import useSWR from 'swr';
 import Input from 'components/Input';
 import { useForm } from 'react-hook-form';
@@ -9,11 +9,17 @@ import useRequest from 'hooks/useRequest';
 import useToast from 'components/Toast/useToast';
 import { useNavigate } from 'react-router-dom';
 import FormItem from 'components/FormItem';
+import { t } from 'i18next';
+import Upload from 'components/Upload';
+import { IFile } from 'hooks/useUpload';
+import Textarea from 'components/Textarea';
 
 interface Props {}
 
 const Edit: React.FC<Props> = () => {
-  const { data: user } = useSWR('/user/me');
+  const { data: user, mutate } = useSWR('/user/me', {
+    revalidateOnFocus: true,
+  });
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [fetcher] = useRequest();
   const [toast] = useToast();
@@ -21,11 +27,19 @@ const Edit: React.FC<Props> = () => {
 
   const {
     register,
+    reset,
+    setValue,
     handleSubmit,
     formState: { errors },
   } = useForm<IUser>({
     defaultValues: user,
   });
+
+  useEffect(() => {
+    reset(user);
+  }, [user]);
+
+  const avatar = user?.avatar ? [{ url: user?.avatar }] : [];
 
   const handleSave = (data: IUser) => {
     setIsSubmitting(true);
@@ -40,6 +54,7 @@ const Edit: React.FC<Props> = () => {
       .then((newUser) => {
         toast('Profile saved!');
         setIsSubmitting(false);
+        mutate(newUser);
         navigate(`/profile/${newUser.id}`);
       })
       .catch((err) => {
@@ -48,18 +63,43 @@ const Edit: React.FC<Props> = () => {
       });
   };
 
+  const handleAvatarChange = (files: IFile[]) => {
+    const file = files[0];
+
+    setValue('avatar', file?.url ?? '');
+  };
+
   return (
     <div className='lanting-profile-edit lanting-page'>
-      <form onSubmit={handleSubmit((data) => handleSave(data))}>
+      <form
+        className='form'
+        onSubmit={handleSubmit((data) => handleSave(data))}
+      >
+        <div className='avatar'>
+          <Upload
+            files={avatar}
+            accept='image/jpg,png,jpeg'
+            onChange={handleAvatarChange}
+            round
+          />
+          <div className='username'>{user?.username}</div>
+        </div>
+
         <FormItem label='Name' name='name' message={errors.name?.message}>
-          <Input {...register('name')} />
+          <Input
+            {...register('name', {
+              required: t('textRequired', { type: t('username') }),
+            })}
+          />
         </FormItem>
         <FormItem label='Bio' name='bio'>
-          <Input {...register('bio')} />
+          <Textarea {...register('bio')} />
         </FormItem>
-        <Button type='submit' disabled={isSubmitting}>
-          Save
-        </Button>
+        <div className='save'>
+          <Button type='submit' disabled={isSubmitting}>
+            Save
+          </Button>
+        </div>
       </form>
     </div>
   );
