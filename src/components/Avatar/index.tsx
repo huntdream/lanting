a148bc2b1ee4bc76873b47a02b9db7cb58d18a60
Yu Mao <maoyulore@outlook.com>
2025-01-10 13:45:47 +0800

    Avatar support custom size

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -5,19 +5,24 @@ import './style.scss';
 
 interface Props {
   src?: string;
-  size?: 'small' | 'large' | 'normal';
+  size?: 'small' | 'large' | 'normal' | number;
   round?: boolean;
   onClick?: () => void;
 }
 
 const Avatar: React.FC<Props> = ({ src, size = 'normal', round, onClick }) => {
+  const isCustomSize = typeof size === 'number';
+
+  const px = isCustomSize ? `${size}px` : undefined;
+
   return (
     <div
       className={cls('lanting-avatar', {
-        [`lanting-avatar-${size}`]: size !== 'normal',
+        [`lanting-avatar-${size}`]: !isCustomSize && size !== 'normal',
         'lanting-avatar-round': round,
         'lanting-avatar-clickable': !!onClick,
       })}
+      style={{ width: px, height: px }}
       onClick={onClick}
     >
       {src ? (
